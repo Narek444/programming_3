8aa0db12834eec65da2193af6966256c34494b1e
Narek444 <narek.beybutyan.g@tumo.org>
2022-02-25 21:41:06 +0400

    lesson_5_1

--- prog_2/script.js
+++ prog_2/script.js
@@ -1,36 +1,5 @@
 
-
-
 let side = 20;
-
-
-
-
-function setup() {
-    frameRate(5);
-    createCanvas(matrix[0].length * side, matrix.length * side);
-    background('#acacac');
-    for (let y = 0; y < matrix.length; y++) {
-        for (let x = 0; x < matrix[y].length; x++) {
-            if (matrix[y][x] == 1) {
-                let gr = new Grass(x, y)
-                grassArr.push(gr)
-            } else if (matrix[y][x] == 2) {
-                let grEat = new GrassEater(x, y)
-                grassEaterArr.push(grEat)
-            } else if (matrix[y][x] == 3) {
-                let grEat = new Predator(x, y)
-                predatorArr.push(grEat)
-            }else if (matrix[y][x] == 4){
-                let Ener = new Energy(x , y)
-                EnergyArr.push(Ener)
-            }else if (matrix[y][x] == 5){
-                let trap = new Trap(x , y)
-                trapArr.push(trap)
-            }
-        }
-    }
-}
 
 function nkarel(matrix) {
 
@@ -60,25 +29,5 @@ function nkarel(matrix) {
             rect(x * side, y * side, side, side);
         }
     }
-    for (var i in grassArr) {
-        grassArr[i].mul()
-    }
-    for (var i in grassEaterArr) {
-        grassEaterArr[i].mul()
-        grassEaterArr[i].eat()
-    }
-    for (var i in predatorArr) {
-        
-        predatorArr[i].mul()
-        predatorArr[i].eat()
-    }
-    for (var i in EnergyArr) {
-        EnergyArr[i].move()
-    }
-    for (var i in trapArr) {
-        trapArr[i].mul()
-        trapArr[i].eat()
-    }
-
 }
 
--- prog_2/server.js
+++ prog_2/server.js
@@ -76,3 +76,72 @@ grassEater = require("./grassEater")
 predator = require("./predator")
 trap = require("./trap")
 energy = require("./energy")
+
+function createObject(matrix) {
+
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                let gr = new Grass(x, y)
+                grassArr.push(gr)
+            } else if (matrix[y][x] == 2) {
+                let grEat = new GrassEater(x, y)
+                grassEaterArr.push(grEat)
+            } else if (matrix[y][x] == 3) {
+                let grEat = new Predator(x, y)
+                predatorArr.push(grEat)
+            } else if (matrix[y][x] == 4) {
+                let Ener = new Energy(x, y)
+                EnergyArr.push(Ener)
+            } else if (matrix[y][x] == 5) {
+                let trap = new Trap(x, y)
+                trapArr.push(trap)
+            }
+        }
+    }
+    io.sockets.emit("send matrix", matrix);
+
+}
+
+function game() {
+    for (var i in grassArr) {
+        grassArr[i].mul()
+    }
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].mul()
+        grassEaterArr[i].eat()
+    }
+    for (var i in predatorArr) {
+
+        predatorArr[i].mul()
+        predatorArr[i].eat()
+    }
+    for (var i in EnergyArr) {
+        EnergyArr[i].move()
+    }
+    for (var i in trapArr) {
+        trapArr[i].mul()
+        trapArr[i].eat()
+    }
+    io.sockets.emit("send matrix", matrix);
+}
+
+setInterval(game, 1000)
+
+io.on('connection', function () {
+    createObject(matrix)
+})
+
+var statistics = {}
+
+setInterval(function(){
+    statistics.Grass = grassArr.length;
+    statistics.grassEater = grassEaterArr.length;
+    statistics.predator = predatorArr.length;
+    statistics.energy = EnergyArr.length;
+    statistics.trap = trapArr.length;
+    fs.writeFile("statistics.json",  JSON.stringify(statistics), function(){
+        console.log("send")
+    })
+},1000)
+
